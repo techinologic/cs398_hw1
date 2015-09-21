2a2e9fbcc7eb48562601e611862c3d5823210a19
Paolo T. Inocencion <paoloinocencion@gmail.com>
2015-09-20 20:03:10 -0400

    Update Main.java
    Edited file for desktop clone testing.

--- src/hw1/Main.java
+++ src/hw1/Main.java
@@ -5,6 +5,8 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("This is a test program to "
 				+ "be published to github.");
+		System.out.println("This was written in the GitHub editor ""
+				+ "and will be pushed back to Desktop.");
 	}
 
 }
