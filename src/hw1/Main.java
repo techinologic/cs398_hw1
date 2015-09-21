bb10bdfa89fcff9f0b9b3274470dbced7e4827ea
Paolo T. Inocencion <paoloinocencion@gmail.com>
2015-09-20 20:03:44 -0400

    Update Main.java

--- src/hw1/Main.java
+++ src/hw1/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("This is a test program to "
 				+ "be published to github.");
-		System.out.println("This was written in the GitHub editor ""
+		System.out.println("This was written in the GitHub editor "
 				+ "and will be pushed back to Desktop.");
 	}
 
